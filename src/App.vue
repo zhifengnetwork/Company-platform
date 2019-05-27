50df2ff70635cdee83d256501fe4d71af79f1c62
hlb243X <2726724651@qq.com>
2019-05-27 10:57:12 +0800

    底部导航

--- src/App.vue
+++ src/App.vue
@@ -20,9 +20,47 @@ export default {
 		padding: 0;
 	}
   a { text-decoration:none }
+	body {
+		background-color: #f7f7f7;
+	}
 	#app {
 	  width: 750px;
 	  height: auto;
 	  overflow-x: hidden;
+	}
+	/*清除浮动代码 '父元素'*/
+	.clearfloat:after{
+		display: block;
+		clear: both;
+		content:" ";
+		visibility: hidden;
+		height: 0;
+	}
+	.clearfloat {
+		zoom: 1;
+	}
+	/*去除ios默认样式*/
+	input button {
+		/*IOS*/
+		-webkit-appearance: none; 
+		background: none;
+		border: none;
+		outline: none;
+	}
+	/*public 省略号*/
+	.publicEllipsis {
+		overflow: hidden;
+		text-overflow: ellipsis;
+		white-space: nowrap;
+	}
+	/*居中*/
+	.centered {
+		position: absolute;
+		top: 0;
+		right: 0;
+		bottom: 0;
+		left: 0;
+		z-index: 1;
+		margin: auto;
 	}
 </style>
--- src/components/index.vue
+++ src/components/index.vue
@@ -3,26 +3,42 @@
 
 		<!--二级页面-->
 		<router-view/>
-		<!--底部导航-->
-		<div class="bottomNavWrap container">
-			<!--项，
-				1、点击时 font-color：class="bNavYseFontColor";
-				2、（未点击时）字体颜色:class="bNavNoFontColor";
-			-->
-			<div class="bottomNavTerm" v-for="(v,i) in data_nav" :key="i" @click.stop="style_fun(i)">
-				<router-link :to="v['url_data']">
-					<!--icon box-->
-					<p class="bottomNavIconBox">
-						<img class="bottomNavIcon" v-show='this_state != i' :src="v['icon'][0]" />
-						<img class="bottomNavIcon" v-show='this_state == i' :src="v['icon'][1]" />
-					</p>
-					<!--title
-						1、（点击时）字体颜色:class="bNavYseFontColor";
-					-->
-					<p class="bottomNavTitle" :class="this_state == i ? 'bNavYseFontColor':'bNavNoFontColor' ">{{v['name']}}</p>
-				</router-link>
+		<!--底部导航 w: 100%-->
+		<div class="bottom_nav_wrap">
+			<!-- w: 90% -->
+			<div class="bottom_nav_box">
+				<!-- 左 -->
+				<div class="bottom_nav_box_left">
+					<!-- 空出左右间距 -->
+					<div class="container bottom_nav_box_left_box">
+						<!-- 项 -->
+						<router-link tag="div" class="container_trem bottom_nav_box_left_trem_box" v-for="(v,i) in data_nav" :to="v.url_data" :key="i" @click.native="style_fun(i)">
+							<p class="bottom_nav_box_left_trem_icon" v-if="v.text == true">
+								<i class="trem_icon" :class="this_state == i ? 'trem_add_icon':'trem_del_icon' "></i>
+							</p>
+							<p class="bottom_nav_box_left_trem_text" :class="this_state == i ? 'bottom_nav_box_left_trem_text_yes':'' "  v-if="v.text == true">{{v.name}}</p>
+							<!-- icon -->
+							<i class="member_icon centered" v-if="v.text == false"></i>
+						</router-link>
+					</div>
+				</div>
+				<!-- 右 (logo) -->
+				<div class="bottom_nav_box_right">
+					<router-link to="/index">
+						<p class="bottom_nav_box_right_trem_icon">
+							<i class="logo_icon"></i>
+						</p>
+						<p class="bottom_nav_box_right_trem_text">分享</p>
+					</router-link>
+				</div>
+
 			</div>
 		</div>
+
+
+
+
+		
 	</div>
 </template>
 
@@ -33,40 +49,30 @@
 		data(){
 			return {
 				/*当前-点击状态*/
-				this_state: 0,
-				/*底部导航-数据*/
+				this_state: null,
+				/*底部导航-数据（没有分享-模块）*/
 				data_nav: [
 					{	
 						name: '首页',
-						url_data: '/index/',
-						icon:[
-							'/static/img/bottomNav/2018100811.png',
-							'/static/img/bottomNav/2018100812.png',
-						]
+						url_data: '/index',
+						/**状态切换-通过index */
+						/* 是否-show-文字 */
+						text: true,
 					},
 					{	
-						name: '待定',
-						url_data: '/index/',
-						icon:[
-							'/static/img/bottomNav/2018100821.png',
-							'/static/img/bottomNav/2018100822.png',
-						]
+						name: '',
+						url_data: '/index',
+						text: false,
 					},
 					{	
-						name: '待定',
-						url_data: '/index/',
-						icon:[
-							'/static/img/bottomNav/2018100831.png',
-							'/static/img/bottomNav/2018100832.png',
-						]
+						name: '购物车',
+						url_data: '/index',
+						text: true,
 					},
 					{	
 						name: '我的',
-						url_data: '/index/user',
-						icon:[
-							'/static/img/bottomNav/2018100841.png',
-							'/static/img/bottomNav/2018100842.png',
-						]
+						url_data: '/index',
+						text: true,
 					},
 				]
 			}
@@ -75,20 +81,29 @@
 		methods: {
 			/*底部样式切换*/
 			style_fun(_index){
-				// console.log(_index);
+				console.log(_index);
 				this.this_state = _index;
 			},
 		},
 		/*钩子函数*/
 		mounted(){
+			/**分享的状态 */
+			var share_start = true;
 			/**根据当前路由--渲染底部导航 */
 			var this_toute = this.$route.path;
 			console.log('当前路由:',this_toute);
 			for(var i=0;i<this.data_nav.length;i++){
-				// console.log(i);
+				console.log(this.data_nav[i]['url_data'], this_toute);
 				if(this.data_nav[i]['url_data'] == this_toute){
 					this.this_state = i;
+					share_start = false;
+					break;
 				}
+			}
+			console.log(this.this_state,share_start);
+			/**分享路径 */
+			if(share_start){
+				this.this_state = null;
 			}
 		}
 		
@@ -101,80 +116,178 @@
 		box-sizing: border-box;
 		-moz-box-sizing: border-box;
 		-webkit-box-sizing: border-box;
-	
-	/*弹性布局*/
-	.container 
-		display: -webkit-box;
-		/* Chrome 4+, Safari 3.1, iOS Safari 3.2+ */
-		display: -moz-box;
-		/* Firefox 17- */
-		display: -webkit-flex;
-		/* Chrome 21+, Safari 6.1+, iOS Safari 7+, Opera 15/16 */
-		display: -moz-flex;
-		/* Firefox 18+ */
-		display: -ms-flexbox;
-		/* IE 10 */
-		display: flex;
-		/* Chrome 29+, Firefox 22+, IE 11+, Opera 12.1/17/18, Android 4.4+ */
-	
-	/*底部导航wrap -s*/
-	.bottomNavWrap 
-		position: fixed;
-		bottom: 0;
-		left: 0;
-		z-index: 9;
-		padding-top: 3px;
-		box-sizing: border-box;
-		width: 100%;
-		height: 98px;
-		font-size: 18px;
-		line-height: 18px;
-		border-top: 1px solid #ededed;
-		background-color: #fff;
-	
-		/*导航 项*/
-		.bottomNavTerm 
-			-prefix-box-flex: 1;
-			-webkit-box-flex: 1;
-			-webkit-flex: 1;
-			-moz-box-flex: 1;
-			-ms-flex: 1;
-			flex: 1;
-			/*width: 2.5rem;*/
-			height: 95px;
-		
-			/*icon box*/
-			.bottomNavIconBox 
-				position: relative;
-				width: 100%;
-				height: 62px;
-			
-				/*icon box=>icon */
-				.bottomNavIcon 
-					display: block;
-					position: absolute;
-					top: 0;
-					right: 0;
-					bottom: 0;
-					left: 0;
-					margin: auto;
-					width: 36px;
-					height: 36px;
-				
-			.bottomNavTitle 
-				width: 100%;
-				font-size: 20px;
-				line-height: 20px;
-				letter-spacing: 2px;
-				text-align: center;
-			
-			/*点击时 font-color*/
-			.bNavYseFontColor 
-				color: #1e82d2;
-			
-			/*不-点击时 font-color*/
-			.bNavNoFontColor 
-				color: #cdcdcd;
-			
-		/*底部导航wrap -e*/
+
+		/*弹性布局 */
+		.container 
+			display: -webkit-box;
+			/* Chrome 4+, Safari 3.1, iOS Safari 3.2+ */
+			display: -moz-box;
+			/* Firefox 17- */
+			display: -webkit-flex;
+			/* Chrome 21+, Safari 6.1+, iOS Safari 7+, Opera 15/16 */
+			display: -moz-flex;
+			/* Firefox 18+ */
+			display: -ms-flexbox;
+			/* IE 10 */
+			display: flex;
+			/* Chrome 29+, Firefox 22+, IE 11+, Opera 12.1/17/18, Android 4.4+ */
+
+			.container_trem
+				-prefix-box-flex: 1;
+				-webkit-box-flex: 1;
+				-webkit-flex: 1;
+				-moz-box-flex: 1;
+				-ms-flex: 1;
+				flex: 1;
+
+		/**底部导航 -s */
+		.bottom_nav_wrap 
+			position: fixed;
+			bottom: 14px;
+			left: 0;
+			z-index: 9;
+			width: 100%;
+			height: 98px;
+
+			.bottom_nav_box 
+				margin: 0 auto;
+				width: 93%;
+				height: 100%;
+
+				.bottom_nav_box_left
+					float: left; 
+					width: 72%;
+					height: 100%;
+					border-radius: 42px
+					border: 2px solid #e6e6e6;
+					background-color: #fff;
+
+					.bottom_nav_box_left_box
+						margin: 0 auto;
+						width: 90%;
+						height: 100%
+						
+						.bottom_nav_box_left_trem_box 
+							position: relative;
+							width: 100%;
+							height: 100%;
+
+							.bottom_nav_box_left_trem_icon
+								padding-top: 13px;
+								box-sizing: border-box;
+								-webkit-box-sizing: border-box;
+								-moz-box-sizing: border-box;
+								height: 57%;
+								// background: red;
+							// 未-点击字体-style
+							.bottom_nav_box_left_trem_text
+								width: 100%;
+								height: 43%;
+								font-size: 16px;
+								line-height 42px;
+								text-align: center;
+								letter-spacing: 1px;
+								color: #7b7b7b;
+								// background: red;
+							// 点击字体-style
+							.bottom_nav_box_left_trem_text_yes
+								color: #ff1132;
+
+						// 首页 icon（style）
+						.bottom_nav_box_left_trem_box:nth-child(1) .trem_icon 
+							display: block;
+							margin: 0 auto;
+							width: 43px;
+							height: 43px;
+							background-repeat: no-repeat;
+							background-position: 0 0;
+							background-size: 100%;
+
+						// 首页 icon (未-点击)
+						.bottom_nav_box_left_trem_box:nth-child(1) .trem_del_icon
+							background-image: url('/static/img/bottomNav/home_iocn-1.png');
+
+						// 首页 icon(点击)
+						.bottom_nav_box_left_trem_box:nth-child(1) .trem_add_icon
+							background-image: url('/static/img/bottomNav/home_iocn-2.png');
+
+						// 会员 icon
+						.bottom_nav_box_left_trem_box:nth-child(2) .member_icon
+							display: block;
+							width: 76px;
+							height: 76px;
+							background-image: url('/static/img/bottomNav/member_icon-1.png');
+							background-repeat: no-repeat;
+							background-position: 0 0;
+							background-size: 100%;
+						
+						// 购物车 icon
+						.bottom_nav_box_left_trem_box:nth-child(3) .trem_icon
+							display: block;
+							margin: 0 auto;
+							width: 46px;
+							height: 43px;
+							background-repeat: no-repeat;
+							background-position: 0 0;
+							background-size: 100%;
+
+						// 购物车 icon (未-点击)
+						.bottom_nav_box_left_trem_box:nth-child(3) .trem_del_icon
+							background-image: url('/static/img/bottomNav/cart_iocn-1.png');
+
+						// 购物车 icon(点击)
+						.bottom_nav_box_left_trem_box:nth-child(3) .trem_add_icon
+							background-image: url('/static/img/bottomNav/cart_iocn-2.png');
+
+						// 我的 icon
+						.bottom_nav_box_left_trem_box:nth-child(4) .trem_icon
+							display: block;
+							margin: 0 auto;
+							width: 38px;
+							height: 43px;
+							background-repeat: no-repeat;
+							background-position: 0 0;
+							background-size: 100%;
+
+						// 购物车 icon (未-点击)
+						.bottom_nav_box_left_trem_box:nth-child(4) .trem_del_icon
+							background-image: url('/static/img/bottomNav/user_icon-1.png');
+
+						// 购物车 icon(点击)
+						.bottom_nav_box_left_trem_box:nth-child(4) .trem_add_icon
+							background-image: url('/static/img/bottomNav/user_icon-2.png');
+
+				.bottom_nav_box_right 
+					float: right;
+					width: 22%;
+					height: 100%;
+					border-radius: 42px
+					border: 2px solid #e6e6e6;
+					background-color: #fff;
+
+					.bottom_nav_box_right_trem_icon
+						padding-top: 13px;
+						box-sizing: border-box;
+						-webkit-box-sizing: border-box;
+						-moz-box-sizing: border-box;
+						height: 57%;
+
+					.bottom_nav_box_right_trem_icon .logo_icon 
+						display: block;
+						margin: 0 auto;
+						width: 49px;
+						height: 43px;
+						background-image: url('/static/img/login_icon.png');
+						background-repeat: no-repeat;
+						background-position: 0 0;
+						background-size: 100%;
+					
+					.bottom_nav_box_right_trem_text
+						width: 100%;
+						height: 43%;
+						font-size: 16px;
+						text-align: center;
+						letter-spacing: 1px;
+						color: #7b7b7b;
+
 </style>
