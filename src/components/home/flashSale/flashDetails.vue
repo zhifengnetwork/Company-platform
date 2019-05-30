fd81cb84aa101fcef8d5ea66be9dc7b84ae53ce6
webhzp <1225613448@qq.com>
2019-05-30 18:46:58 +0800

    限时购

--- src/components/home/flashSale/flashDetails.vue
+++ src/components/home/flashSale/flashDetails.vue
@@ -45,19 +45,23 @@
                 <!-- 配送地区 -->
                 <div class="sendArea">
                     <van-cell-group class="goods-cell-group">
-                        <van-cell is-link>
+                        <van-cell is-link @click="areaSelect">
                             <template slot="title">
                                 <span class="label">配送</span>
-                                <span class="text">广州白云区</span>
+                                <span class="text">{{this.address}}</span>
                             </template>
                         </van-cell>
+                       
+                        <van-popup v-model="areaShow" position="bottom">
+                             <van-area :area-list="areaList" @confirm="onAddrConfirm" @cancel="onAddrCancel" />
+                        </van-popup>
                     </van-cell-group>
                 </div>
 
                 <!-- 运费 -->
                 <div class="postArea">
                     <van-cell-group class="goods-cell-group">
-                        <van-cell is-link >
+                        <van-cell is-link  @click="freightShow = true">
                             <template slot="title">
                                 <span class="label">运费</span>
                                 <span class="text">免运费</span>
@@ -69,12 +73,65 @@
                 <!-- 优惠券 -->
                 <div class="detailCoupons">
                     <van-cell-group class="goods-cell-group">
-                        <van-cell is-link >
+                        <van-cell is-link @click="couponShow = true">
                             <template slot="title">
                                 <span class="label">优惠券</span>
                                 <span class="text" style="float:right">领券</span>
                             </template>
                         </van-cell>
+                        
+                        <!-- 领取优惠券上拉菜单 -->
+                        <van-actionsheet v-model="couponShow" title="优惠券" >
+                            <div class="coupon-list">
+                                <ul>
+                                    <li>
+                                        <div class="full-reduction">
+                                            <div class="coupon-amount">5<span class="unit">元</span></div>
+                                            <div class="coupon-condition">
+                                                <p>满100减5元</p>
+                                            </div>
+                                        </div>
+                                        <div class="couponInfo">
+                                            <h2 class="coupon-name">商品优惠劵</h2>
+                                            <p class="validity-date">有效期：2019.05.10-2019.05.20</p>
+                                            <span class="useBtn">去使用</span>
+                                        </div>
+                                    </li>
+                                    <li>
+                                        <div class="full-reduction">
+                                            <div class="coupon-amount">200<span class="unit">元</span></div>
+                                            <div class="coupon-condition">
+                                                <p>满1000减200元</p>
+                                            </div>
+                                        </div>
+                                        <div class="couponInfo">
+                                            <h2 class="coupon-name">商品优惠劵</h2>
+                                            <p class="validity-date">有效期：2019.05.10-2019.05.20</p>
+                                            <span class="useBtn">去使用</span>
+                                        </div>
+                                    </li>
+                                    <li class="new">
+                                        <div class="full-reduction">
+                                            <div class="coupon-amount">300<span class="unit">元</span></div>
+                                            <div class="coupon-condition">
+                                                <p>满100减5元</p>
+                                            </div>
+                                        </div>
+                                        <div class="couponInfo">
+                                            <h2 class="coupon-name">新人专享</h2>
+                                            <p class="validity-date">有效期：2019.05.10-2019.05.20</p>
+                                            <span class="useBtn">去使用</span>
+                                        </div>
+                                    </li>
+                                </ul>
+                            </div>
+                            <!-- 暂无可使用优惠券 -->
+                            <div class="none" style="display:none">
+                                <img src="/static/img/public/none.png" >
+                                <p>暂无可使用优惠券</p>
+                            </div>
+                        </van-actionsheet>
+
                     </van-cell-group>
                 </div>
 
@@ -84,7 +141,7 @@
                         <van-cell is-link>
                             <template slot="title">
                                 <span class="label">规格</span>
-                                <span class="wayText" style="float:right">默认规格显示</span>
+                                <span class="wayText" style="float:right">S M L XL</span>
                             </template>
                         </van-cell>
                     </van-cell-group>
@@ -94,8 +151,12 @@
 
             <!-- tab切换 -->
             <div class="tab-content">
-                <van-tabs v-model="active">
-                    <van-tab title="商品详情">商品详情</van-tab>
+                <van-tabs v-model="tabActive">
+                    <van-tab title="商品详情">
+                        <div class="details-wrap">
+                            <img src="/static/img/flashSale/00details-img01.png" />
+                        </div>
+                    </van-tab>
                     <van-tab title="参数">
                         <div class="params-wrap">
                             <ul class="param-list">
@@ -143,7 +204,7 @@
                                         </div>
                                         <div class="score">
                                             <van-rate
-                                                v-model="value"
+                                                v-model="rateVal"
                                                 icon="like-o"
                                                 void-icon="like-o"
                                                 color="#f70a0a"
@@ -168,7 +229,7 @@
                                         </div>
                                         <div class="score">
                                             <van-rate
-                                                v-model="value"
+                                                v-model="rateVal"
                                                 icon="like-o"
                                                 void-icon="like-o"
                                                 color="#f70a0a"
@@ -193,7 +254,7 @@
                                         </div>
                                         <div class="score">
                                             <van-rate
-                                                v-model="value"
+                                                v-model="rateVal"
                                                 icon="like-o"
                                                 void-icon="like-o"
                                                 color="#f70a0a"
@@ -218,7 +279,7 @@
                                         </div>
                                         <div class="score">
                                             <van-rate
-                                                v-model="value"
+                                                v-model="rateVal"
                                                 icon="like-o"
                                                 void-icon="like-o"
                                                 color="#f70a0a"
@@ -235,6 +296,23 @@
                     </van-tab>
                 </van-tabs>
             </div>
+
+        </div>
+        
+        <!-- 商品规格 -->
+        <div class="sku-wrap">
+            <van-actionsheet v-model="skuShow" title="商品规格">
+                <div class="sku-content">
+                    <div class="sku-row">
+                        <div class="sku-row-title">尺寸</div>
+                        <div class="sku-row-con">
+                            <span class="sku-row-item">S</span>
+                            <span class="sku-row-item">M</span>
+                            <span class="sku-row-item">L</span>
+                        </div>
+                    </div>
+                </div>
+            </van-actionsheet>
         </div>
 
         <!-- 底部菜单 -->
@@ -251,10 +329,12 @@
                 <goods-action-big-btn
                 type="warning"
                 text="加入购物车"
+                @click="handleBtn"
                 />
                 <goods-action-big-btn
                 type="danger"
                 text="马上抢购"
+                @click="handleBtn"
                 />
             </van-goods-action>
         </div>
@@ -264,24 +344,53 @@
 
 <script>
 import Vue from 'vue'
-import {GoodsAction,GoodsActionBigBtn,GoodsActionMiniBtn,Toast} from 'vant';
+import AreaList from './area'
+import {GoodsAction,GoodsActionBigBtn,GoodsActionMiniBtn,Toast, } from 'vant'
+
 Vue.use(
-GoodsAction,
-GoodsActionBigBtn,
-GoodsActionMiniBtn,
-Toast,);
+    GoodsAction,
+    GoodsActionBigBtn,
+    GoodsActionMiniBtn,
+    Toast,
+    )
+
 export default {
     name:'flashDetails',
     components:{
         GoodsAction,
         GoodsActionBigBtn,
         GoodsActionMiniBtn,
+   
     },
     data(){
         return {
-            active: 2,
-            value: 3
+            tabActive: 0,//tab选中
+            rateVal: 3,//评分当前分值
+            areaShow:false,//省市区上拉
+            address:'广州白云区',//地址
+            areaList:AreaList,// 指定数据源
+            couponShow:false,//优惠券上拉菜单
+            skuShow:false,//规格
         }
+    },
+    methods:{
+        // 省市区上拉
+        areaSelect(){
+            this.areaShow = true
+        },
+        //配送地区确定选择
+        onAddrConfirm(val){  
+            this.areaShow = false;
+            this.address = val[0].name+ val[1].name +val[2].name
+        },
+        //配送地区取消选择
+        onAddrCancel(){  
+            this.areaShow = false
+        },
+        // 显示规格
+        handleBtn(){
+            this.skuShow = true
+        }  
     }
 }
 </script>
@@ -371,10 +480,71 @@ export default {
                         display inline-block 
             .van-hairline--top-bottom::after
                 border none
+            .select-wrap /deep/ .van-cell__value
+                flex none
+            // 优惠券
+            .detailCoupons
+                .van-actionsheet__content
+                    padding-top 88px
+                .van-actionsheet__header
+                    width 100%
+                    height 88px
+                    background #ffffff
+                    position absolute
+                    top 0
+                    left 0
+                .coupon-list
+                    height 600px
+                    overflow-y auto
+                    li
+                        width 714px
+                        height 222px
+                        font-size 30px
+                        display flex
+                        align-items center
+                        margin 0 auto
+                        background url("~/static/img/flashSale/coupon-bg.png") no-repeat
+                        background-size 100%
+                        margin-bottom 14px
+                        &.new
+                            background url("~/static/img/flashSale/new-coupon-bg.png") no-repeat
+                            background-size 100%
+                        .full-reduction
+                            width 260px
+                            color #ffffff
+                            text-align center
+                            margin-right 30px
+                            .coupon-amount
+                                font-size 90px
+                                .unit
+                                    font-size 30px
+                        .couponInfo
+                            color #383838
+                            .coupon-name
+                                font-size 30px
+                                margin-bottom 20px
+                            .validity-date
+                                font-size 24px
+                                margin-bottom 30px
+                            .useBtn
+                                width 366px
+                                height 50px
+                                line-height 50px
+                                font-size 24px
+                                text-align center
+                                color #ffffff
+                                background #ff5f58
+                                display block
+                                border-radius 5px
+                .none
+                    text-align center
+                    margin 50px auto
+                    img
+                        width 80px
+                    p
+                        font-size 24px
+                        line-height 50px
         .tab-content
-            .van-tabs >>> .van-tabs__content
-                padding 0 25px
-                box-sizing border-box
             .van-tabs >>> .van-hairline--top-bottom::after
                 border none
             .van-tabs >>> .van-tabs__content
@@ -385,9 +555,16 @@ export default {
                 background url("/static/img/flashSale/line-right.png") right  center no-repeat
                 background-size 2px 27px
             .van-tabs >>> .van-tab:last-child span
-                background none 
+                background none
+            // 商品详情
+            .details-wrap
+                img
+                    width 100% 
+            // 参数
             .params-wrap
                 width 100%
+                padding 0 25px
+                box-sizing border-box
                 .param-list
                     li  
                         height 58px
@@ -410,7 +587,10 @@ export default {
                             flex 1
                             padding-left 28px
                             box-sizing border-box
+            // 商品评论
             .comment-wrap
+                padding 0 25px
+                box-sizing border-box
                 .comment-list
                     li
                         color #4a4949
@@ -446,7 +626,22 @@ export default {
                         .eval-content
                             p
                                 font-size 18px
-
+    .sku-wrap
+        .sku-content
+            padding 0 25px
+            box-sizing border-box
+            .sku-row-title
+                font-size 30px
+                color #151515
+            .sku-row-con
+                .sku-row-item
+                    width 160px
+                    height 40px
+                    line-height 40px
+                    background-color #f3f3f3
+                    text-align center
+                    display inline-block
+                
 
     .bottom-bar
         .van-goods-action
