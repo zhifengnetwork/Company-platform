69314caec069a8feea10cea6d717d18dc4faf884
webhzp <1225613448@qq.com>
2019-06-05 11:08:23 +0800

    手机号绑定

--- src/components/user/bindingTel.vue
+++ src/components/user/bindingTel.vue
@@ -3,23 +3,37 @@
         <!-- 头部 -->
         <headtop></headtop>
         <div class="content">
-            <div class="input-group">
-                <input type="number" placeholder="请输入11位手机号">
-            </div>
-            <div class="input-group">
-                <input type="text" placeholder="输入验证码">
-                <button class="getVerifyCode">获取验证码</button>
+            <div class="form-group">            
+                <div class="input-group">
+                    <input type="number" v-model="tel" placeholder="请输入11位手机号">
+                </div>
+                <div class="input-group">
+                    <input type="text" placeholder="输入验证码">
+                    <button class="getVerifyCode" :disabled="disabled" @click="getVerifyCode()">{{verifyCode}}</button>
+                </div>
             </div>
         </div>
     </div>
 </template>
 
 <script>
+import Vue from 'vue'
 import headtop from '@/components/public/page_top_head'
+
+import { Toast } from 'vant'
+Vue.use(Toast)
+
 export default {
     name:'bindingTel',
     components: {
         headtop
+    },
+    data(){
+        return{
+            tel:'',
+            disabled:false,//获取验证码按钮可点击，true不可点击
+            verifyCode:"获取验证码"
+        }
     },
      /*组件实例创建完成，属性已绑定，但DOM还未生成*/
     created: function(){
@@ -58,6 +72,50 @@ export default {
         /**改变vuex对应头部数据 */
         this.$store.commit('change_head',style_obj);
     },
+    methods:{
+        // 倒计时
+        countDown(){
+            var count = 60
+            var timer = setInterval( () => {
+                if(count == 0){
+                    clearInterval(timer)
+                    this.verifyCode = '重新获取'
+                    this.disabled = false;
+                }
+                // 补零操作
+                else if(count < 10){
+                    count = '0' + count
+                    this.verifyCode = count + "秒后重试";
+                    this.disabled = true;
+                    count -- ;
+                }else{
+                    this.verifyCode = count + "秒后重试";
+                    this.disabled = true;
+                    count -- ;
+                }
+            },1000);
+        },
+        // 验证手机号码
+        validatePhone(){
+            if(!this.tel){
+                Toast('手机号不能为空')
+                return false;
+            }else if(!/^1[345678]\d{9}$/.test(this.tel)){
+                Toast('请填写正确的手机号码')
+                return false;
+            }else{
+                return true;
+            }
+        },
+        // 获取验证码
+        getVerifyCode(){
+            if(this.validatePhone()){
+                // 发送网络请求
+                this.countDown();
+            }
+        },
+
+    }
 }
 </script>
 
@@ -68,26 +126,34 @@ export default {
     .content
         padding 108px 24px
         box-sizing border-box
-        .input-group
-            height 56px
-            display flex
-            align-items center
-            border-bottom 1px solid #e6e6e6
-            padding-top 22px
-            padding-bottom 10px
-            input
-                flex 1
-                height 30px
+        .form-group
+            box-shadow 1px 1px 8px #ccc
+            padding 30px 
+            box-sizing border-box
+            .input-group
+                height 56px
+                display flex
+                align-items center
+                border-bottom 1px solid #e6e6e6
+                padding-bottom 10px
+                margin-bottom 25px
+                input
+                    font-size 24px
+                    flex 1
+                    height 60px
+                    border none
+                .getVerifyCode
+                    font-size 28px
+                    color #ff4902
+                    padding 0 10px
+                    height 60px
+                    line-height 60px
+                    border 1px solid #ff4902
+                    font-size 28px
+                    background none
+                    border-radius 25px
+            .input-group:last-child
                 border none
-            .getVerifyCode
-                padding 0 10px
-                height 60px
-                line-height 60px
-                border 1px solid #ff4902
-                font-size 28px
-                background none
-                border-radius 25px
-        .input-group:last-child
-            border none
+                margin 0
 
 </style>
