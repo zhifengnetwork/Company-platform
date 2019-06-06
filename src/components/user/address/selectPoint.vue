6fb96fde266ba6408afebb55d6c545d065b9537d
webhzp <1225613448@qq.com>
2019-06-06 15:00:07 +0800

    收货地址

--- src/components/user/address/selectPoint.vue
+++ src/components/user/address/selectPoint.vue
@@ -54,9 +54,11 @@ export default {
             // 接收位置信息，用户选择确认位置点后选点组件会触发该事件，回传用户的位置信息
             var loc = event.data;
             if (loc && loc.module == 'locationPicker') {//防止其他应用也会向该页面post信息，需判断module是否为'locationPicker'
-                console.log('location', loc.poiaddress,loc.poiname);
+                // console.log('location', loc.poiaddress,loc.poiname);
+                // console.log('location', loc);
                 // 跳转路由
-                that.$router.push({name:'addAddress',params:{'poiaddress':loc.poiaddress,'poiname':loc.poiname}})
+                // that.$router.push({name:'addAddress',params:{'poiaddress':loc.poiaddress,'poiname':loc.poiname}})
+                that.$router.push({name:'addAddress',params:{'location':loc}})
             }
         }, false);
     },
